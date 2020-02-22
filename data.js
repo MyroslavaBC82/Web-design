9812c47989587873a4ae2abdaf18d37c65f97211
MyroslavaBC82 <60743318+MyroslavaBC82@users.noreply.github.com>
2020-02-22 20:16:35 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+var todayDate = new Date();
+var currYear = todayDate.getFullYear();
+var currMonth = todayDate.getMonth()+1;
+var currDay = todayDate.getDate();
+document.write("Сегодняшняя дата: ");
+document.write(currDay + "-" + currMonth + "-" + currYear);
+</script>
