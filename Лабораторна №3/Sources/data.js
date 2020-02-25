c53cf6907bdbedfc237aa37d282ac1640f62876b
MyroslavaBC82 <lyubchenko.miroslava@gmail.com>
2020-02-25 16:02:09 +0300

    Initial commit.

--- Лабораторна №3/Sources/data.js
+++ Лабораторна №3/Sources/data.js
@@ -1,8 +1,1 @@
-<script type="text/javascript">
-var todayDate = new Date();
-var currYear = todayDate.getFullYear();
-var currMonth = todayDate.getMonth()+1;
-var currDay = todayDate.getDate();
-document.write("Сегодняшняя дата: ");
-document.write(currDay + "-" + currMonth + "-" + currYear);
-</script>
+<script>alert("Nice to meet you")</script>
